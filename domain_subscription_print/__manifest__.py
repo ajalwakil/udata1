ab8bafaa61410bb057c2042750d66a4419906e3d
itechresourcesadmin <info.itrpk@gmail.com>
2022-07-27 11:03:47 +0500

    [Fields] Domain Name Fields in customer subscription Portal view, also add default Electronic Invoice.

---
+++ domain_subscription_print/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Domain Subscription Print",
+    'summary': """ """,
+    'description': """ """,
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '15.1',
+    'depends': ['base','sale_subscription'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/domain_subscription_views.xml',
+    ],
+
+}
