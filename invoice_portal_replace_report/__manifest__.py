ebfcc749092c7e4be321f5b5c0336759d3010aa9
itechresourcesadmin <info.itrpk@gmail.com>
2022-07-27 11:08:18 +0500

    [Fields] Replace default Invoice with Electronic Invoice for Portal users.

---
+++ invoice_portal_replace_report/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Invoice portal invoice',
+    'version': '14.0.0.1',
+    'summary': 'Invoice portal replace download button report',
+    'description': 'Invoice portal replace download button report',
+    'depends': ['portal','account','ksa_e_invoive'],
+    # 'data': ['views/account_portal_templates.xml'],
+    'installable': True,
+    'auto_install': False,
+}
