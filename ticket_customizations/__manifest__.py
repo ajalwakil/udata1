5229c0250a5385df66b3498194225536d11fbd20
octagon9 <ebattota@gmail.com>
2022-12-19 13:01:38 +0500

    [UI] ticket customization

---
+++ ticket_customizations/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Ticket Customizations",
+    'summary': """ """,
+    'description': """ """,
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    # any module necessary for this one to work correctly
+    'depends': ['base','helpdesk','website_helpdesk_form'],
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+}
