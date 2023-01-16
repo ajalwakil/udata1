a702c13c8693169a8c8dc0927e7a91146327f9fc
octagon9 <ebattota@gmail.com>
2023-01-16 18:43:43 +0500

    [UI] add subscription changes

--- subscription_customizations/models/models.py
+++ subscription_customizations/models/models.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 class SaleSubscriptionInherit(models.Model):
     _inherit = 'sale.subscription'
 
-    trigger_date = fields.Date(string='Trigger Date', tracking=True)
+    trigger_date = fields.Date(string='Trigger Date',readonly=False, tracking=True,compute='_compute_trigger_date')
+
+    @api.depends('date')
+    def _compute_trigger_date(self):
+        self.trigger_date = False
+        if self.date:
+            self.trigger_date = self.date + relativedelta(months=-1)
