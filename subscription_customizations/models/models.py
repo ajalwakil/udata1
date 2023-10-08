47dce9ed53f287d87bee301376032fc7e17255aa
octagon9 <ebattota@gmail.com>
2023-10-08 15:20:42 +0500

    [UI] update

--- subscription_customizations/models/models.py
+++ subscription_customizations/models/models.py
@@ -29,10 +29,61 @@ class SaleSubscriptionInherit(models.Model):
         today = fields.Date.today()
 
         # Find subscriptions with an expired subscription date
-        expired_subscriptions = self.search([('date', '=', today),('stage_id.category', '=', 'progress')])
-        if expired_subscriptions:
-            for subscription in expired_subscriptions:
+        subscriptions_created = self.env['sale.subscription'].search(['&',
+                                                                      ('date_start', '=', today),
+                                                                      ('stage_id.category', '=', 'progress')
+                                                                      ])
+
+        if subscriptions_created:
+            for subscription_c in subscriptions_created:
                 # Send a payment reminder email to the subscriber
-                template = self.env.ref('subscription_customizations.email_payment_expired')
-                if template:
-                    template.send_mail(subscription.id, force_send=True)
+                template_c = self.env.ref('subscription_customizations.email_subscription_created')
+                if template_c:
+                    template_c.send_mail(subscription_c.id, force_send=True)
+
+        subscriptions_30_days = self.env['sale.subscription'].search(['&',
+                                                                      ('trigger_date', '=', today),
+                                                                      ('stage_id.category', '=', 'progress')
+                                                                      ])
+        if subscriptions_30_days:
+            for subscription_30 in subscriptions_30_days:
+                # Send a payment reminder email to the subscriber
+                template_30 = self.env.ref('subscription_customizations.email_payment_reminder_before_one_month')
+                if template_30:
+                    template_30.send_mail(subscription_30.id, force_send=True)
+
+        subscriptions_15_days = self.env['sale.subscription'].search(['&',
+                                                                      ('trigger_date_15', '=', today),
+                                                                      ('stage_id.category', '=', 'progress')
+                                                                      ])
+
+        if subscriptions_15_days:
+            for subscription_15 in subscriptions_15_days:
+                # Send a payment reminder email to the subscriber
+                template_15 = self.env.ref('subscription_customizations.email_payment_reminder_before_15_days')
+                if template_15:
+                    template_15.send_mail(subscription_15.id, force_send=True)
+
+        subscriptions_3_days = self.env['sale.subscription'].search(['&',
+                                                                     ('trigger_date_3', '=', today),
+                                                                     ('stage_id.category', '=', 'progress')
+                                                                     ])
+
+        if subscriptions_3_days:
+            for subscription_3 in subscriptions_3_days:
+                # Send a payment reminder email to the subscriber
+                template_3 = self.env.ref('subscription_customizations.email_payment_reminder_before_3_days')
+                if template_3:
+                    template_3.send_mail(subscription_3.id, force_send=True)
+
+        expired_subscriptions = self.env['sale.subscription'].search(['&',
+                                                                      ('date', '=', today),
+                                                                      ('stage_id.category', '=', 'progress')
+                                                                      ])
+
+        if expired_subscriptions:
+            for subscription_exp in expired_subscriptions:
+                # Send a payment reminder email to the subscriber
+                template_exp = self.env.ref('subscription_customizations.email_payment_expired')
+                if template_exp:
+                    template_exp.send_mail(subscription_exp.id, force_send=True)
