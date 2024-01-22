632cd076169379e4eaf1cd29358752de1fdedf06
Hafiz Junaid <99405647+hafizjunaid663@users.noreply.github.com>
2024-01-22 13:25:36 +0500

    Update

---
+++ inventory_cron/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Inventory Cron",
+    'summary': """ """,
+    'description': """ """,
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    # any module necessary for this one to work correctly
+    'depends': ['base','product'],
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+}
