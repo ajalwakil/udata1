7728f3baf0a92b3df4a22ccdeb82ad49a991d6df
Hafiz Junaid <99405647+hafizjunaid663@users.noreply.github.com>
2024-01-29 12:01:28 +0500

    Update

---
+++ udata_crm/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Udata Crm",
+    'summary': """ """,
+    'description': """ """,
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+    # any module necessary for this one to work correctly
+    'depends': ['base','sale_crm','udata_advanced'],
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+}
